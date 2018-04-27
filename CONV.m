3c7ab36e2de514535188172e95368b31787b8db5
Patrick Skeba <ptskeba@gmail.com>
2018-04-27 13:15:40 -0400

    working for 32x32x3

--- CONV.m
+++ CONV.m
@@ -1,4 +1,4 @@
-function conv_filt = CONV(x,filt_spec)
+function conv_filt = CONV(x,weights)
 % x is row of original data (1,3072 uint8)
 % w is weight vector, b is bias
 % filt_spec - [w,f,p,s]
@@ -6,32 +6,41 @@ function conv_filt = CONV(x,filt_spec)
 x_color_square = reshape(x,[32,32,3]); % THIS ARRANGES BY COLUMNS INSTEAD OF ROWS!!
 x_color_square = permute(x_color_square,[2,1,3]); % this should fix it
 
-filt_size = 4; % allows 8 full sweeps
-stride = 2; % skip
+width = size(x_color_square,1);
 
-K = (32 - filt_size)/stride + 1; % number of receptive fields
+filt_size = 8;
+stride = 2;
 
-w = zeros(K^2,1,3);
+K = (size(x,1) - filt_size)/stride + 1; % number of receptive fields
 
+receptive_columns = zeros(K^2,filt_size^2,3);
 
+% Not sure about generalizability, but this works for 32x32 with 8,2
+for i = 1:size(x_color_square,3)
+    X = reshape(x_color_square(:,:,i),[width,width]);
+    A = im2col(X,[filt_size,filt_size]);
+    B = A(:,1:stride:end);
+    C = [];
+    for j = 1:2*K-1:size(B,2)
+        C = [C B(:,j:j+K-1)];
+    end
+    receptive_columns(:,:,i) = C.'; % transpose for easy multiplication
+end
 
+conv_filt = zeros(K^2,1);
+for i = 1:size(receptive_columns,3)
+   conv_filt = conv_filt + receptive_columns(:,:,i) * weights; 
+end
 
-
-% % make receptive layer into a column test
-% %col = reshape(x_color_square(1:4,1:4,:),[48,1]);
-% receptive_columns = zeros(filt_size^2*3,K^2);
-% 
-% % This matrix contains 1 column for each receptive field. The column is a
-% % squished version of the filt_size*filt_size*3 field: first are red values
-% % (layer 1), then green, then blue. The first filt_size values in the
-% % vector are the first column of the red values; next filt_size values are
-% % the second column, then third column. 
-% for i = 0:K-1
-%     for j = 0:K-1
-%         receptive_columns(:,i*2+1 + j) = ...
-%             reshape(x_color_square(i*2+1:i*2+4,j*2+1:j*2+4,:),[filt_size^2*3,1]);
-%     end
+conv_filt = reshape(conv_filt,[K,K]);
+% Correct for 32x32!
+% X = reshape(1:32^2,[32,32]);
+% A = im2col(X,[8,8]);
+% B = A(:,1:2:625);
+% C = [];
+% for i = 1:25:313
+% C = [C B(:,i:i+12)];
 % end
 
-% not really what this will be
-conv_filt = receptive_columns;
+
+
